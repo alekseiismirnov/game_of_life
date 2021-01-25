6d0c028d3e6fc3ba885aef7161b27b081d1ee25c
Alekseii Smirnov <alekseii.sm@gmail.com>
2021-01-25 04:27:10 +0200

    makes a field and can set and get cells values

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -1,4 +1,16 @@
 class GameOfLife
   def initialize(rows, cols)
+    @rows = rows
+    @cols = cols
+
+    @grid = Array.new(rows, Array.new(cols, false))
+  end
+
+  def get(row, col)
+    return @grid[row][col]
+  end
+
+  def set(row, col)
+    @grid[row][col] = true
   end
 end
--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -5,4 +5,19 @@ describe GameOfLife do
   it 'generates 1x1 grid' do
     expect(GameOfLife.new(1,1)).to be_truthy
   end
+
+  context 'with 10x10 grid' do
+    before(:example) do
+      @game = GameOfLife.new(10,10)
+    end
+
+    it 'gets false, when not set' do
+      expect(@game.get(5,6)).to eq(false)
+    end
+
+    it 'could populate a cell' do
+      @game.set(5,6)
+      expect(@game.get(5,6)).to eq(true)
+    end
+  end
 end
